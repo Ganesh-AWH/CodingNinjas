420958aff090f3ed190881f731af29f595b434f6
Ganesh <ganesh02003@gmail.com>
2024-02-26 15:44:56 +0530

    sort list 0s,1s,2s

---
+++ sortList.java
@@ -0,0 +1,68 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node sortList(Node head) {
+        // Write your code here
+        //Not movable
+        Node zeroHead = new Node(-1);
+        Node oneHead = new Node(-1);
+        Node twoHead=  new Node(-1);
+
+        //movable
+        Node zero = zeroHead;
+        Node one = oneHead;
+        Node two = twoHead;
+
+        Node temp = head;
+        while(temp!=null){
+            if(temp.data == 0){
+                zero.next = temp;
+                zero = zero.next;
+            }else if(temp.data == 1){
+                one.next = temp;
+                one = one.next;
+            }else{
+                two.next = temp;
+                two = two.next;
+            }
+            temp = temp.next;
+        }
+
+        //use pen and paper to understand
+        zero.next = oneHead.next!=null?oneHead.next:twoHead.next;
+        //works when one is present 
+        one.next = twoHead.next;
+
+        two.next = null;
+
+        return zeroHead.next;
+    }
+}
