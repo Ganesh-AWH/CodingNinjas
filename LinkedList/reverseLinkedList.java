031e5633692b349eaeb1155eff0fe6e0a480efda
Ganesh <ganesh02003@gmail.com>
2024-02-26 15:50:54 +0530

    reverse the linked list iterative

---
+++ LinkedList/reverseLinkedList.java
@@ -0,0 +1,42 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+ public class Solution
+ {
+     public static Node reverseLinkedList(Node head)
+     {
+         // Write your code here.
+         //approach using recursion
+         if(head == null || head.next == null) return head;
+         Node newHead = reverseLinkedList(head.next);
+         Node front = head.next;
+         front.next = head;
+         head.next = null;
+         return newHead;
+     }
+ }
+ 
